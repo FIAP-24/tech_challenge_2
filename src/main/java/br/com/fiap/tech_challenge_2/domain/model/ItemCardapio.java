bb8d1a4fe469b6ab3f5b7a72171f1292c1bd04e9
Mikael Luz <mikael.luz@southsystem.com.br>
2025-07-31 18:18:25 -0300

    Inclusão de DELETE e testes unitários

--- src/main/java/br/com/fiap/tech_challenge_2/domain/model/ItemCardapio.java
+++ src/main/java/br/com/fiap/tech_challenge_2/domain/model/ItemCardapio.java
@@ -1,6 +1,6 @@
 package br.com.fiap.tech_challenge_2.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
