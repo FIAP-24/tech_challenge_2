0fda42a8e278fd93e3c279fc4abb52ce33320d6e
Renan da S. Silvaira <silveira.s.renan@gmail.com>
2025-08-03 21:41:55 -0300

    Modificação force delete SQL query

--- src/main/java/br/com/fiap/tech_challenge_2/infrastructure/persistence/repository/ItemCardapioJpaRepository.java
+++ src/main/java/br/com/fiap/tech_challenge_2/infrastructure/persistence/repository/ItemCardapioJpaRepository.java
@@ -2,6 +2,9 @@ package br.com.fiap.tech_challenge_2.infrastructure.persistence.repository;
 
 import br.com.fiap.tech_challenge_2.infrastructure.persistence.entity.ItemCardapioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +12,13 @@ import java.util.List;
 @Repository
 public interface ItemCardapioJpaRepository extends JpaRepository<ItemCardapioEntity, Long> {
 
+    @Modifying
+    @Query("DELETE FROM ItemCardapioEntity i WHERE i.id = :id")
+    void deleteItemById(@Param("id") Long id);
+
+
+
     List<ItemCardapioEntity> findByRestauranteId(Long restauranteId);
     
     List<ItemCardapioEntity> findByRestauranteIdAndDisponivelApenasNoLocalFalse(Long restauranteId);
-} 
+}
